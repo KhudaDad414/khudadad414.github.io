4c80c87967f4e224125901f507580814a5b04f40
Khuda Dad Nomani <khudadad414@gmail.com>
2024-07-05 21:50:03 +0100

    create main page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Nomani\'s Corner';
+export const SITE_DESCRIPTION = "Welcome to Nomani's Corner! I'm Khuda Dad Nomani, and this is where I share my thoughts, experiences, and insights through blog posts. Whether it's about technology, personal growth, or anything else that sparks my curiosity, you'll find it here. Join me on this journey of exploration and discovery!!";
